69ab28632a1528443d9f34a6fff89e57a2b5baf1
lichao <184078718@qq.com>
2023-08-18 14:22:36 +0800

    增加unplugin优化

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import {createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
 import router from './router/index'
 import './plugins/qiankun'
-import "element-plus/dist/index.css";
 import './styles/main.scss'
-createApp(App).use(createPinia()).use(ElementPlus).use(router).mount('#app_main')
+createApp(App).use(createPinia()).use(router).mount('#app_main')
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import { fileURLToPath } from "node:url"
+import AutoImport from "unplugin-auto-import/vite"
+import Components from "unplugin-vue-components/vite"
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +12,26 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url))
     }
   },
-  plugins: [vue()],
+  plugins: [
+    vue(), // ...
+    AutoImport({
+      // 这里除了引入 vue 以外还可以引入pinia、vue-router、vueuse等，
+      // 甚至你还可以使用自定义的配置规则，见 https://github.com/antfu/unplugin-auto-import#configuration
+      imports: ["vue"],
+      // 第三方组件库的解析器
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      // dirs 指定组件所在位置，默认为 src/components
+      // 可以让我们使用自己定义组件的时候免去 import 的麻烦
+      dirs: ["src/components/"],
+      // 配置需要将哪些后缀类型的文件进行自动按需引入
+      extensions: ["vue", "md"],
+      // 解析的 UI 组件库，这里以 Element Plus 和 Ant Design Vue 为例
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
   server: {
     // proxy: {
     //   "/junminrongheApi": {
